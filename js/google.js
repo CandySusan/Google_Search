4238a4a55972f0bf4781af1a560ebd35aef0071d
Candy Susan <candysusan55@gmail.com>
2018-11-02 01:42:58 +0300

    Add Validations [Delivers #161653458]

--- js/google.js
+++ js/google.js
@@ -1,5 +1,9 @@
-document.getElementById('userInput').addEventListener('keypress', function (event) {
-    if (event.keyCode == 13) {
-        event.preventDefault();
+function check_input() {
+    var input = document.getElementById("userInput").value;
+    if (input == "") {
+        alert("Type something");
+        return false;
+    } else {
+        alert(input)
     }
-});
+} 
